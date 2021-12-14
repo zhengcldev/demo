0698d92e523d56ad2d71b452cb79c0a04aa488dd
zhengchuanlong <zhengcl0818@163.com>
2021-12-14 12:34:36 +0800

    [v1.0] 项目初始化

---
+++ src/main/java/com/algorithm/demonstration/DemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.algorithm.demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonstrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemonstrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/algorithm/demonstration/DemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithm.demonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
